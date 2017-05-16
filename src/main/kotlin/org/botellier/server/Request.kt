a1e551306d4e5f188df5e9cf9d8b34f94d95c76a
Daniel Rivas <ers.daniel+dev@gmail.com>
2017-05-16 02:58:18 -0500

    Better dispatcher organization

--- src/main/kotlin/org/botellier/server/Request.kt
+++ src/main/kotlin/org/botellier/server/Request.kt
@@ -8,34 +8,36 @@ data class Request(val client: Client, val command: Command)
 class RequestDispatcher(val server: Server) {
     fun dispatch(request: Request) {
         val writer = request.client.socket.getOutputStream()
-
+        val isAuthenticated = !server.requiresPassword() || request.client.isAuthenticated
         try {
-            if (request.command is AuthCommand) {
-                val result = request.command.execute(server, request.client)
-                writer.write(result.toByteArray())
-            }
-            else if (request.command is QuitCommand) {
-                val result = request.command.execute(server, request.client)
-                writer.write(result.toByteArray())
-                request.client.socket.close()
-            }
-            else if (!server.requiresPassword() || request.client.isAuthenticated) {
-                when (request.command) {
-                    is ConnCommand -> {
-                        val result = request.command.execute(server, request.client)
-                        writer.write(result.toByteArray())
-                    }
-                    is StoreCommand -> {
-                        val result = request.command.execute(server.dbs[request.client.dbIndex])
-                        writer.write(result.toByteArray())
-                    }
-                    else -> {
-                        throw Command.CommandException("Invalid command.")
+            when {
+                request.command is AuthCommand -> {
+                    val result = request.command.execute(server, request.client)
+                    writer.write(result.toByteArray())
+                }
+                request.command is QuitCommand -> {
+                    val result = request.command.execute(server, request.client)
+                    writer.write(result.toByteArray())
+                    request.client.socket.close()
+                }
+                isAuthenticated -> {
+                    when (request.command) {
+                        is ConnCommand -> {
+                            val result = request.command.execute(server, request.client)
+                            writer.write(result.toByteArray())
+                        }
+                        is StoreCommand -> {
+                            val store = server.dbs[request.client.dbIndex]
+                            val result = request.command.execute(store)
+                            writer.write(result.toByteArray())
+                        }
+                        else -> {
+                            throw Command.CommandException("Invalid command.")
+                        }
                     }
                 }
-            }
-            else {
-                throw Command.CommandException("Not authenticated. Use AUTH command.")
+                else ->
+                    throw Command.CommandException("Not authenticated. Use AUTH command.")
             }
         }
         catch (e: Command.WrongTypeException) {
